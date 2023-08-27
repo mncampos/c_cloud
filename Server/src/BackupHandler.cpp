db8b916da29d51655c7bec29e5fcbf113b669324
Leonardo Bilhalva <leobbbilhalva@gmail.com>
2023-08-27 04:06:26 -0300

    feat(BackupHandler): added file to deal with messages reception on backup server

--- Server/src/BackupHandler.cpp
+++ Server/src/BackupHandler.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include "../headers/Server.hpp"
 #include "../headers/BackupHandler.hpp"
 
 BackupHandler::BackupHandler(int backupSocket, ServerSocket *serverSocket)
@@ -5,6 +7,21 @@ BackupHandler::BackupHandler(int backupSocket, ServerSocket *serverSocket)
     this->backupSocket = backupSocket;
     this->serverSocket = serverSocket;
 }
+
+// void *heartbeatCheck(void *arg) //@TODO: apagar se nao usar thread abaixo
+// {
+//     Server *server = reinterpret_cast<Server *>(arg);
+
+//     while (true)
+//     {
+//         Packet receivedPacket = server->serverSocket.receiveMessage(server->backupSocket.getSocketFd());
+//         if (receivedPacket.type == HEARTBEAT)
+//         {
+//             std::cout << "[+] RESPONDAAAAA." << std::endl;
+//         }
+//         sleep(5);
+//     }
+// }
 
 void BackupHandler::handleBackup()
 {
@@ -16,7 +33,19 @@ void BackupHandler::handleBackup()
         // fazer uma funcao q verifica se o sinal esta ativo e atualiza todas replicas
         // o conteudo é tudo que esta no servidor principal
 
-        // @TODO: heartbeat
-        // recebeu mensagem tipo heartbeat que backup envia para principal -> servidor principal  devolve mensagem do tipo heartbeat
+        // @TODO: precisa criar essa thread para heartbeat?
+        // pthread_t heartbeatCheckThread;
+        // if (pthread_create(&heartbeatCheckThread, nullptr, heartbeatCheck, reinterpret_cast<void *>(this)) != 0)
+        // {
+        //     std::cerr << "[-] Heartbeat Check thread creation fail!" << std::endl;
+        // }
+
+        Packet pkt = this->serverSocket->receiveMessage(this->backupSocket);
+        if (pkt.type == HEARTBEAT)
+        {
+            std::cout << "[-] Receive Heartbeat from backup and responding it!" << std::endl;
+            this->serverSocket->sendMessage(this->backupSocket, Packet(HEARTBEAT));
+        }
+        sleep(5);
     }
 }
