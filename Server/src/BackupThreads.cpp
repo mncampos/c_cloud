7a32d197c824994561de3bec107ce4aba14c9448
Leonardo Bilhalva <leobbbilhalva@gmail.com>
2023-08-26 23:54:02 -0300

    test(heatbeat): test if get into function

--- Server/src/BackupThreads.cpp
+++ Server/src/BackupThreads.cpp
@@ -7,6 +7,11 @@ void *backupElectionSocket(void *arg)
 
 void *backupHeartBeat(void *arg)
 {
+    while (true)
+    {
+        sleep(10);
+        cout << "pao de batata";
+    }
 }
 
 void *backupDataSync(void *arg)
